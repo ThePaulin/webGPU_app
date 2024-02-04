026ab4b99fbd64d7e7ab5ddb3c692e77540abc04
ThePaulin <kubwirapaulin@gmail.com>
2024-02-04 15:19:52 -0500

    rendering solid color

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -8,34 +9,59 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
+  async function handleWebGPU() {
+
+    const canvas = window.document.querySelector("canvas");
+     
+      console.log("navigator: ", navigator);
+      const adapter = await navigator.gpu.requestAdapter();
+      if (!adapter) {
+        throw new Error("No appropriate GPUAdapter found.");
+      };
+
+      const device = await adapter.requestDevice();
+      const context = canvas?.getContext("webgpu");
+      const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+      context.configure({
+        device: device,
+        format: canvasFormat
+      });
+
+      const encoder = device.createCommandEncoder();
+
+      const pass = encoder.beginRenderPass({
+        colorAttachments: [{
+          view: context.getCurrentTexture().createView(),
+          loadOp: "clear",
+          storeOp: "store",
+          clearValue: {r:0, g:0, b: 0.9, a:1},
+        }]
+      });
+      pass.end();
+      
+    const commandBuffer = encoder.finish();
+    device.queue.submit([commandBuffer]);
+
+    // finish the command buffer and immediately submit it.
+    device.queue.submit([encoder.finish()]);
+  };
+
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      handleWebGPU();
+    };
+
+   
+  }, [])
+
+  // webGPU code here!!
+  
+   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <h1>webGPU app built with Remix framework</h1>
+        <canvas width="512" height="512"></canvas>
+
+      </div>
   );
 }

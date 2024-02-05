fe4e464d2f78a17b65fcf9a1b28063b40f86f94d
ThePaulin <kubwirapaulin@gmail.com>
2024-02-05 00:09:31 -0500

    rendering red square on canvas

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -37,6 +37,78 @@ export default function Index() {
           clearValue: {r:0, g:0, b: 0.9, a:1},
         }]
       });
+  
+
+     const cellShaderModule = device.createShaderModule({
+      label: "Cell shader",
+      code: `
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f) -> 
+          @builtin(position) vec4f {
+          return vec4f(pos,0,1); // (X, Y, Z, W)
+        }
+        
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f {
+          return vec4f(1,0,0,1); // (r,g,b,a)  
+        }
+      `
+    });
+
+ /* define the vertex layout */
+    const vertexBufferLayout = {
+      arrayStride: 8,
+    attributes: [{
+      format: "float32x2",
+        offset: 0,
+        shaderLocation: 0, // Position, see vertex shader
+      }],
+    };
+    
+
+     const cellPipeline = device.createRenderPipeline({
+      label: "Cell cellPipeline",
+      layout: "auto",
+      vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout],
+      },
+      fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+          format: canvasFormat
+        }]
+      }
+
+    });
+
+const vertices = new Float32Array([
+      -0.8,-0.8, // triangle 1 
+      0.8,-0.8,
+      0.8,0.8,
+
+      -0.8,-0.8, // triangle 2
+      0.8, 0.8,
+      -0.8,0.8,
+    ]);
+
+    const vertexBuffer = device.createBuffer({
+      label: "Cell vertices",
+      size: vertices.byteLength,
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+
+     device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+
+       // Draw square
+    pass.setPipeline(cellPipeline);
+    pass.setVertexBuffer(0, vertexBuffer);
+    pass.draw(vertices.length /2 ); // 6 vertices
+
+
       pass.end();
       
     const commandBuffer = encoder.finish();
@@ -44,7 +116,13 @@ export default function Index() {
 
     // finish the command buffer and immediately submit it.
     device.queue.submit([encoder.finish()]);
-  };
+
+       
+   
+    // Shaders
+
+   
+        };
 
 
   useEffect(() => {
